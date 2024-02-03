14f3e6bd752e7578f7136eeeb61d3f554b744d9b
Hayden <hayden26@live.com>
2024-02-03 13:45:50 +0400

    files

---
+++ README.txt
@@ -0,0 +1,5 @@
+Hayden Jovan Fernandes - M00847442
+
+GitHub Repository Link: https://github.com/Darx26/CW1
+
+GitHub Pages Link: https://darx26.github.io/CW1/afterSchoolActivities.html
---
+++ lessons.js
@@ -0,0 +1,92 @@
+let lessons = [
+    {
+        "id": 1,
+        "subject": "Art",
+        "location": "Block 19, Knowledge Park, Dubai",
+        "price": 20.00,
+        "image": "images/art.png",
+        "availableInventory": 5,
+        "rating": 4
+    },
+    {
+        "id": 2,
+        "subject": "Robotics",
+        "location": "Block 16, Knowledge Park, Dubai",
+        "price": 40.00,
+        "image": "images/robotics.png",
+        "availableInventory": 5,
+        "rating": 5
+    },
+    {
+        "id": 3,
+        "subject": "Football",
+        "location": "Block 15, Knowledge Park, Dubai",
+        "price": 20.00,
+        "image": "images/football.png",
+        "availableInventory": 5,
+        "rating": 2
+    },
+    {
+        "id": 4,
+        "subject": "Tennis",
+        "location": "Block 16, Knowledge Park, Dubai",
+        "price": 10.00,
+        "image": "images/tennis.png",
+        "availableInventory": 5,
+        "rating": 4
+    },
+    {
+        "id": 5,
+        "subject": "Cricket",
+        "location": "Block 17, Knowledge Park, Dubai",
+        "price": 30.00,
+        "image": "images/cricket.png",
+        "availableInventory": 5,
+        "rating": 3
+    },
+    {
+        "id": 6,
+        "subject": "Basketball",
+        "location": "Block 17, Knowledge Park, Dubai",
+        "price": 20.00,
+        "image": "images/basketball.png",
+        "availableInventory": 5,
+        "rating": 3
+    },
+    {
+        "id": 7,
+        "subject": "Swimming",
+        "location": "Block 19, Knowledge Park, Dubai",
+        "price": 40.00,
+        "image": "images/swimming.png",
+        "availableInventory": 5,
+        "rating": 2
+    },
+    {
+        "id": 8,
+        "subject": "Volleyball",
+        "location": "Block 15, Knowledge Park, Dubai",
+        "price": 25.00,
+        "image": "images/volleyball.png",
+        "availableInventory": 5,
+        "rating": 3
+    },
+    {
+        "id": 9,
+        "subject": "Music",
+        "location": "Block 15, Knowledge Park, Dubai",
+        "price": 30.00,
+        "image": "images/music.png",
+        "availableInventory": 5,
+        "rating": 5
+    },
+    {
+        "id": 10,
+        "subject": "Dance",
+        "location": "Block 15, Knowledge Park, Dubai",
+        "price": 30.00,
+        "image": "images/dance.png",
+        "availableInventory": 5,
+        "rating": 5
+    },
+]
